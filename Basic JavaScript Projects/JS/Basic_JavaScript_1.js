982c38cc8bee7b66e77e2505a90ac42cde53e7ca
ScDavis27 <64049331+ScDavis27@users.noreply.github.com>
2020-05-06 17:27:21 -0400

    Add folder via upload
    Add folder via upload per assignment on page 58

---
+++ Basic JavaScript Projects/JS/Basic_JavaScript_1.js
@@ -0,0 +1,16 @@
+var X="What's up, world?";
+window.alert(X);
+
+document.write(4*4);
+
+var Family="The Davis'", Dad="Kenneth", Mom="Carol", Son1="Kenny", Son2="Scott";
+var Dad=Dad.fontcolor("red");
+document.write(Dad);
+
+var B= "Florida";
+var A= "Gators";
+var B=B.fontcolor("orange");
+var A=A.fontcolor("blue");
+document.write(B + A)
+
+
