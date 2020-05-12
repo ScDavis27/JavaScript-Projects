d29d28ebe6cfec623cacc959f7848803437a8d25
ScDavis27 <64049331+ScDavis27@users.noreply.github.com>
2020-05-12 11:17:25 -0400

    Add files via upload
    Add JavaScript project folder via upload per assignment on page 140

--- Basic JavaScript Projects/JS/Example_1.js
+++ Basic JavaScript Projects/JS/Example_1.js
@@ -7,3 +7,9 @@ function count_Function() {
         return Starting_point;
     }
 }
+
+function get_Date() {
+    if (new Date().getHours() < 18) {
+    document.getElementById("Greeting").innerHTML = "How are you today?";
+    }
+}
