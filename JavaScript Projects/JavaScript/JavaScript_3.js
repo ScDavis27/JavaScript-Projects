eb2a38f5084b8ad048f26a4a560c7afac556b077
ScDavis27 <64049331+ScDavis27@users.noreply.github.com>
2020-05-20 21:21:31 -0400

    add files via upload
    add files via upload

--- JavaScript Projects/JavaScript/JavaScript_3.js
+++ JavaScript Projects/JavaScript/JavaScript_3.js
@@ -2,3 +2,4 @@ function displayType(quarterback) {
     var quarterbackType = quarterback.getAttribute("data-quarterback");
     alert(quarterbackType + " is the Quarterback for the " + quarterback.innerHTML);
 }
+
---
+++ JavaScript Projects/JavaScript/validateForm.js
@@ -0,0 +1,15 @@
+function validateForm() {
+    var x = document.forms["myForm"]["fname"].value;
+    if (x == "") {
+        alert("Name must be filled out");
+        return false;
+    }
+}
+
+function validateForm() {
+    var x = document.forms["myForm"]["phonenumber"].value;
+    if (x == "") {
+        alert("Name must be filled out");
+        return false;
+    }
+}
